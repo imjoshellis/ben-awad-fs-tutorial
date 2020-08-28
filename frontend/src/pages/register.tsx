e6e6c3e9f3f8eced29eb56a204cf1122078dc0ef
Josh Ellis <josh@imjoshellis.com>
2020-08-28 12:09:06 -0500

    Add Formik

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { Formik, Form } from 'formik'
+import { FormControl, FormLabel, Input } from '@chakra-ui/core'
 
 interface RegisterProps {}
 
-export const Register: React.FC<RegisterProps> = () => <>Register Page</>
+export const Register: React.FC<RegisterProps> = () => (
+  <>
+    <Formik
+      initialValues={{ username: '', password: '' }}
+      onSubmit={v => console.log(v)}
+    >
+      {({ values, handleChange }) => (
+        <Form>
+          <FormControl>
+            <FormLabel htmlFor='username'>Username</FormLabel>
+            <Input
+              value={values.username}
+              onChange={handleChange}
+              id='username'
+              placeholder='username'
+            />
+            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+          </FormControl>
+        </Form>
+      )}
+    </Formik>
+  </>
+)
 
 export default Register
