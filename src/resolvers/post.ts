19d3f1c25d1d529a163ce6b37a608802ae994d69
Josh Ellis <josh@imjoshellis.com>
2020-08-27 20:23:18 -0500

    Setup full CRUD for posts

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -9,11 +9,44 @@ export class PostResolver {
     return em.find(Post, {})
   }
 
+  @Query(() => Post, { nullable: true })
+  post (@Arg('id') id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
+    return em.findOne(Post, { id })
+  }
+
   @Query(() => Post)
-  post (
-    @Arg('id', () => Int) id: number,
+  async createPost (
+    @Arg('title') title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title })
+    await em.persistAndFlush(post)
+    return post
+  }
+
+  @Query(() => Post)
+  async updatePost (
+    @Arg('id') id: number,
+    @Arg('title') title: string,
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
-    return em.findOne(Post, { id })
+    const post = await em.findOne(Post, { id })
+    if (!post) {
+      return null
+    }
+    if (typeof title !== undefined) {
+      post.title = title
+      await em.persistAndFlush(post)
+    }
+    return post
+  }
+
+  @Query(() => Post)
+  async deletePost (
+    @Arg('id') id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Boolean> {
+    await em.nativeDelete(Post, { id })
+    return true
   }
 }
