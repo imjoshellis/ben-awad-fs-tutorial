aca487ed2ece15c86dbbdc296d8050cde26265ef
Josh Ellis <josh@imjoshellis.com>
2020-08-28 11:25:55 -0500

    Add types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,13 @@
 import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core'
 import { Response, Request } from 'express'
 
+export type MyContextArgs = {
+  req: Request & { session: Express.Session }
+  res: Response
+}
+
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
-  req: Request
+  req: Request & { session: Express.Session }
   res: Response
 }
